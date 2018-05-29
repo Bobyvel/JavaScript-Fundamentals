038adef623ab45f542ff2e323c13dc2720e0d443
Boby <32449183+Bobyvel@users.noreply.github.com>
2018-05-29 11:41:11 +0300

    Revert "matrix and strings lab"
    This reverts commit 77607dac25fbf9ae1bd9c0e10b9899c1bc13b21b.

--- String/10.MatchAllWords.js
+++
@@ -1,9 +0,0 @@
-function matchWords(input) {
-    let pattern = /\w+/g;
-
-    let result = input.match(pattern);
-
-    console.log(result.join('|'));
-}
-
-matchWords('A Regular Expression needs to have the global flag in order to match all occurrences in the text');
